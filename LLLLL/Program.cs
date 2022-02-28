eb2d82249ad29861dd3837bff30c4abab0c3790a
LTFYR <lutfiyariea@code.edu.az>
2022-02-28 09:17:06 +0400

    Add project files.

---
+++ LLLLL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LLLLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LLLLL/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LLLLL
+{
+    internal class Student
+    {
+        public string Fullname;
+        public int GroupNo;
+        public int Age;
+
+        public Student(string fullname,int groupNo,int age)
+        {
+            Fullname = fullname;
+            GroupNo = groupNo;
+            Age = age;
+        }
+        public bool static CheckGroup(string groupNo)
+        {
+            if (groupNo.Length == 4) ;
+
+            bool.IsUpper == false;
+            bool.IsDigit=false;
+
+            foreach (var item in groupNo)
+
+            {
+                if (char.IsUpper(item))
+                    IsUpper = true;
+                continue;
+
+            }
+            else (char.IsDigit(item))
+                    IsDigit = true;
+            continue;
+
+            bool result = IsUpper && IsDigit;
+            return result;
+        }
+       
+    }
+
+
+}
